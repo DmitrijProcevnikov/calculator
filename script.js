0e68d63405ffc7a24d86ef41ecbac26989cb1736
Dmitriy <dmitrijprocevnikov@gmail.com>
2023-04-04 17:07:07 +0300

    calculator

---
+++ script.js
@@ -0,0 +1,59 @@
+// Создаем контейнер для калькулятора
+const calcContainer = document.createElement("div");
+calcContainer.classList.add("calc-container");
+
+// Создаем элементы для вывода значения
+const outputValue = document.createElement("div");
+outputValue.classList.add("output-value");
+outputValue.textContent = "0";
+
+// Создаем кнопки
+const btns = [
+  { text: "1", className: "btn-number" },
+  { text: "2", className: "btn-number" },
+  { text: "3", className: "btn-number" },
+  { text: "C", className: "btn-clear" },
+  { text: "4", className: "btn-number" },
+  { text: "5", className: "btn-number" },
+  { text: "6", className: "btn-number" },
+  { text: "*", className: "btn-operation" },
+  { text: "7", className: "btn-number" },
+  { text: "8", className: "btn-number" },
+  { text: "9", className: "btn-number" },
+  { text: "/", className: "btn-operation" },
+  { text: ".", className: "btn-number" },
+  { text: "0", className: "btn-number" },
+  { text: "+", className: "btn-operation" },
+  { text: "-", className: "btn-operation" },
+  { text: "=", className: "btn-equal" },
+];
+
+// Добавляем элементы на страницу
+document.body.appendChild(calcContainer);
+calcContainer.appendChild(outputValue);
+
+btns.forEach((btn) => {
+  const button = document.createElement("button");
+  button.classList.add("btn", btn.className);
+  button.textContent = btn.text;
+  button.addEventListener("click", () => handleButtonClick(btn.text));
+  calcContainer.appendChild(button);
+});
+
+// Обработчик нажатия на кнопку
+function handleButtonClick(value) {
+  switch (value) {
+    case "C":
+      outputValue.textContent = "0";
+      break;
+    case "=":
+      outputValue.textContent = eval(outputValue.textContent);
+      break;
+    default:
+      if (outputValue.textContent === "0") {
+        outputValue.textContent = value;
+      } else {
+        outputValue.textContent += value;
+      }
+  }
+}
